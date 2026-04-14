4001eeea253b02a761c9368bd58a604cc5f719ab
OhSorryBro <1michal.domagala@gmail.com>
2026-04-14 12:52:32 +0200

    numbers_in_python

---
+++ Numbers.py
@@ -0,0 +1,17 @@
+2+3
+3-2
+2*3
+3/2
+3**2
+2+3*2
+0.2+0.1
+my_age = 14_0000_0000
+print(my_age)
+x,y,z = 0,0,0
+print(x,y,z)
+print(4+4)
+print(9-1)
+print(4*2)
+print(16/2)
+fav_num=13
+print(f'My favourite number is {fav_num}')
