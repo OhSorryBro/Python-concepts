c92b3a665331444e214ce5c4b1f8cebcc4dad9c6
Michal Domagała <maxisu1708@Interia.pl>
2026-04-17 20:19:00 +0200

    4_loops

--- 4_Loops.py
+++ 4_Loops.py
@@ -7,3 +7,14 @@ for magician in magicians:
     print(f'We are waiting for your next magic spectatle, dear {magician.title()}.\n')
 
 print('Thank you everyone. That was a trully amazing expirience!')
+
+pizza =['a','b','c']
+for pizza in pizza:
+    print(f' I like eating {pizza}')
+print('I really love pizza a because its first in the alphabet. Pizza b is my least favourite because beeing 2nd is the worst place. Pizza c is third pizza, it is nice to stand on the podium')
+print("I do absolutly love pizza!")
+mammals=['gepard','Donkey','monkey']
+for mammal in mammals:
+    print(f'{mammal.title()} is a very useful animal')
+
+print('All mentioned animals are simply wonderful!')
