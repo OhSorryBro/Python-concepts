b0e0ee5c10d23bda32fa254c79db8f9ae2f666e8
Michal Domagała <maxisu1708@Interia.pl>
2026-04-19 10:29:11 +0200

    5_IFs_ex5

--- 5_Ifs_ex1.py
+++ 5_Ifs_ex1.py
@@ -13,3 +13,55 @@ car.lower()=='subaru'.lower()
 numbers=range(0,10)
 10 in numbers
 11 not in numbers
+
+age=19
+if age>=18:
+    print(f'You can vote.')
+    print('Did you already registered so you will be able to vote?')
+else:
+    print("Sorry you are too young to be able to vote")
+    print("You can register when you are at least 18")
+
+age=12
+if age<4:
+    print("Ticket price is 0")
+elif age<18:
+    print("Ticket price is 25")
+else:
+    print("Ticket price is 40")
+
+if age<4:
+    price = 0
+elif age<18:
+    price = 25
+elif age<65:
+    price =40
+else:
+    price =20
+
+print(f'Ticket price is {price}')
+
+requested_toppings =['muschrooms','double cheese']
+if 'muschrooms' in requested_toppings:
+    print("Adding muschrooms")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni")
+if 'double cheese' in requested_toppings:
+    print("Adding double cheese")
+print("Pizza is ready!")
+print('=================')
+
+available_toppings =['mushrooms','olive','bacon','pepperoni','pineapple','double cheese']
+
+requested_toppings =['mushrooms','bacon','double cheese','fries']
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        if requested_topping in available_toppings:
+            print(f'Adding {requested_topping}.')
+        else:
+            print(f'We do not have an option to add {requested_topping}.')
+else:
+    print('Are you sure that you wish to order pizza without any toppings?')
+
+print("\nYour pizza is ready")
+
---
+++ 5_Ifs_ex2.py
@@ -0,0 +1,59 @@
+alien_color='green'
+if alien_color=='green':
+    print('You earned 5 points')
+alien_color='yellow'
+if alien_color=='green':
+    print('You earned 5 points')
+
+alien_color='green'
+if alien_color == 'green':
+    print('You earned 5 points')
+elif alien_color =='yellow':
+    print("You earned 10 points")
+elif alien_color =='red':
+    print('You earned 15 points')
+
+alien_color='yellow'
+if alien_color == 'green':
+    print('You earned 5 points')
+elif alien_color =='yellow':
+    print("You earned 10 points")
+elif alien_color =='red':
+    print('You earned 15 points')
+
+
+alien_color='red'
+if alien_color == 'green':
+    print('You earned 5 points')
+elif alien_color =='yellow':
+    print("You earned 10 points")
+elif alien_color =='red':
+    print('You earned 15 points')
+
+
+age =3
+if age<2:
+    print('You are newborn')
+elif age>=2 and age<4:
+    print('You are a child that has learned walking')
+elif age>=4 and age<13:
+    print('You are just a child')
+elif age>=13 and age<20:
+    print('You are a teenager')
+elif age>=20 and age<65:
+    print('You are an adult')
+else:
+    print('You are a senior')
+
+favourite_fruits=['apple','banana','pear']
+for fruit in favourite_fruits:
+    if fruit== 'banana':
+        print("You really like bananas!")
+    if fruit=='apple':
+        print("You really like apples!")
+    if fruit=='strawberry':
+        print("You really like strawberries!")
+    if fruit=='pear':
+        print("You really like pears!")
+    if fruit=='orange':    
+        print("You really like oranges!")
