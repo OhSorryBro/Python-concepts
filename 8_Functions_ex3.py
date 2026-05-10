d03607b73c4bb334f0fa6392f32920e7d4432624
Michal Domagała <maxisu1708@Interia.pl>
2026-05-10 13:05:47 +0200

    8_Functions

---
+++ 8_Functions_ex3.py
@@ -0,0 +1,26 @@
+# TRY IT YOURSELF
+# 8.12. Sandwiches. 
+# Write a function that accepts a list of items a customer wants on a sandwich. 
+# The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that's being ordered. 
+# Call the function three times, using a different number of arguments each time.
+
+
+
+# 8.13. User Profile. 
+# Start with a copy of the user_profile.py program from earlier in this chapter. 
+# Build a profile of yourself by calling build_profile(), using your first and last names and three other key-value pairs that describe you.
+print("=====")
+
+
+
+
+# 8.14. Cars. 
+# Write a function that stores information about a car in a dictionary. The function should always receive a manufacturer and a model name. 
+# It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. 
+# Your function should work for a call like this one:
+# pythoncar = make_car('subaru', 'outback', color='blue', tow_package=True)
+# Print the dictionary that's returned to make sure all the information was stored correctly.
+print("=====")
+
+
+
