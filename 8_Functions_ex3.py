90141854160a6a219434f7e821e92b4e0caacded
Michal Domagała <maxisu1708@Interia.pl>
2026-05-10 13:59:00 +0200

    8_Functions_ex3

--- 8_Functions_ex3.py
+++ 8_Functions_ex3.py
@@ -3,17 +3,26 @@
 # Write a function that accepts a list of items a customer wants on a sandwich. 
 # The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that's being ordered. 
 # Call the function three times, using a different number of arguments each time.
+def make_sandwich(*items):
+    print(f"Preparing sandwich with {items}")
 
-
-
+make_sandwich("Onion")
+make_sandwich("Onion","butter")
+make_sandwich("Onion","butter",'jam')
 # 8.13. User Profile. 
 # Start with a copy of the user_profile.py program from earlier in this chapter. 
 # Build a profile of yourself by calling build_profile(), using your first and last names and three other key-value pairs that describe you.
 print("=====")
 
+def build_profile (first, last, **user_info):
+    """Building dictionary containing all user information."""
+    user_info['first name'] = first
+    user_info['last name'] = last
+    return user_info
 
-
-
+build_profile('Dodo','Bobo',Age = 12,Status = 'Yelly', Color = 'Green')
+build_profile('Mr. Evil','From Saint house',Group = 'Crazy', Status = 'Unknown', Color = 'Unknown')
+build_profile("Rachet", "Silverhand", Age = 639, Status = "Dead", Color = "Rotten-white")
 # 8.14. Cars. 
 # Write a function that stores information about a car in a dictionary. The function should always receive a manufacturer and a model name. 
 # It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. 
@@ -22,5 +31,11 @@ print("=====")
 # Print the dictionary that's returned to make sure all the information was stored correctly.
 print("=====")
 
+def make_car(manufacturer, model, **kwargs):
+    """Building dictionary containing all car information"""
+    kwargs['manufacturer'] = manufacturer
+    kwargs['model'] = model
+    return kwargs
 
-
+pythoncar_1 = make_car('subaru', 'outback', color='blue', tow_package=True)
+print(pythoncar_1)
