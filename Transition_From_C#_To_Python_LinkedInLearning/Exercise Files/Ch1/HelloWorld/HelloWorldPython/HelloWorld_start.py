8d49b4ae8f266304f3272c959aa25ea4b0b5a83f
OhSorryBro <1michal.domagala@gmail.com>
2026-05-13 13:51:48 +0200

    LinkedInLearning

--- Transition_From_C#_To_Python_LinkedInLearning/Exercise Files/Ch1/HelloWorld/HelloWorldPython/HelloWorld_start.py
+++
@@ -1,4 +0,0 @@
-# Example HelloWorld file for the Python for the C# Developer LinkedIn Learning course
-
-
-print("Hello World")
--- Transition_From_C#_To_Python_LinkedInLearning/Exercise Files/Ch5/Scopes/ScopesPy/scopes_start.py
+++
@@ -1,3 +0,0 @@
-# Exercise file for Python for the C# Developer LinkedIn Learning course by Joe Marini
-
-# TODO: Python uses the "with" statement to define a scope block
