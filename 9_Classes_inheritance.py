484039d29d4e1e67f2f6424c9bd738c3d4eb4797
Michal Domagała <maxisu1708@Interia.pl>
2026-05-14 13:18:30 +0200

    9_Classes_inheritance

--- 9_Classes_inheritance.py
+++ 9_Classes_inheritance.py
@@ -40,6 +40,7 @@ class ElectricCar(Car):
         """Inicialisation of class attributes."""
         super().__init__(make, model, year)
         self.battery_size = 40
+        self.battery = Battery()
 
     def describe_battery(self):
         """Printing information about size of the battery."""
@@ -49,7 +50,31 @@ class ElectricCar(Car):
         """Electric car does not have gas tank."""
         print(f"This car cannot fill it's gas tank.")
 
+class Battery:
+    """Simple model of an electric car battery."""
+    
+    def __init__(self,battery_size=40):
+        """"""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Printing information about battery size."""
+        print(f"This car has battery that is {self.battery_size} kWh.")
+
+    def get_range(self):
+        """Print actual information about range based on the size of the battery."""
+        if self.battery_size == 40:
+            range = 150
+        elif self.battery_size == 65:
+            range = 225
+
+        print(f"Range of this car is approx. {range} when the battery is full.")
+
+
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
 my_leaf.describe_battery()
 my_leaf.fill_gas_tank()
+
+my_leaf.battery.describe_battery()
+my_leaf.battery.get_range()
---
+++ 9_classes_ex3.py
@@ -0,0 +1,29 @@
+# 9.6 – Ice Cream Stand
+# An ice cream stand is a specific type of restaurant. Define a class called IceCreamStand inheriting from the Restaurant class created in exercise 9.1 or 9.4. Any version of the class will work. 
+# Add an attribute called flavors that stores a list of ice cream flavors. Define a method called display_flavors that displays the available flavors. Create an instance of the IceCreamStand class and call the new method.
+
+
+
+
+# 9.7 – Admin
+# An administrator is a special type of user. Define a class called Admin inheriting from the User class created in exercise 9.3 or 9.5. 
+# Add a privileges attribute storing a list of strings such as "can add post", "can delete post", or "can ban user". Define a method called show_privileges() that displays the list of admin privileges. 
+# Create an instance of the Admin class and call the new method.
+
+
+
+
+# 9.8 – Privileges
+# Define a separate class called Privileges. This class should have one attribute (privileges) storing a list of strings, as shown in the previous exercise. 
+# Move the show_privileges() method to the new class. Create an instance of Privileges as an attribute in the Admin class. Then create a new instance of Admin and use show_privileges() to display the privileges.
+
+
+
+
+# 9.9 – Battery Upgrade
+# Start from the latest version of the electric_car.py program created in this section. Add a new method called upgrade_battery() to the Battery class. 
+# This method should check the battery capacity and set it to 65 if it is currently different. 
+# Create an electric car instance with a default battery capacity, call get_range(), then call upgrade_battery(), and call get_range() again. You should notice an increase in the car's range.
+
+
+
